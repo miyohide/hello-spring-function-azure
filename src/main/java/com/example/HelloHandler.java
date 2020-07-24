f3e23861ad0ec0006202334a1adf656620c97b80
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-24 22:59:16 +0900

    create handler for Azure Function.

---
+++ src/main/java/com/example/HelloHandler.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import com.example.model.Greeting;
+import com.example.model.User;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
+
+import java.util.Optional;
+
+public class HelloHandler extends AzureSpringBootRequestHandler<User, Greeting> {
+    @FunctionName("hello")
+    public HttpResponseMessage execute(
+            @HttpTrigger(
+                    name = "request",
+                    methods = {HttpMethod.GET, HttpMethod.POST},
+                    authLevel = AuthorizationLevel.ANONYMOUS
+            ) HttpRequestMessage<Optional<User>> request,
+            ExecutionContext context) {
+        context.getLogger().info("Greeting user name: " + request.getBody().get().getName());
+        return request
+                .createResponseBuilder(HttpStatus.OK)
+                .body(handleRequest(request.getBody().get(), context))
+                .header("Content-Type", "application/json")
+                .build();
+    }
+}
